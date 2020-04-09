f799dec03ef307aaca92378eb59fc9dd5554a9dc
pythonpete32 <pythonpete32@gmail.com>
2020-04-09 13:51:30 +0100

    refactor setupAragon

--- src/setupAragon.js
+++ src/setupAragon.js
@@ -1,43 +1,39 @@
 #!/usr/bin/env node
-/* eslint-disable global-require */
-// TODO: Refactor setupAragon
-// Refactor to the file exports the function for testing
-
-// TODO: Create unit tests for# setupAragon()
-// crete unit test for the functiom
 const fs = require('fs');
 
 const args = process.argv.slice(2);
-const INFURA_KEY = args[0];
-const PRIVATE_KEY = args[1];
+const userName = require('os').userInfo().username;
 
-const keySettings = JSON.stringify(
-	{
-		rpc: `https://rinkeby.infura.io/v3/${INFURA_KEY}`, // ?+
-		keys: [`${PRIVATE_KEY}`], // ?+
-	},
-	null,
-	2,
-);
+const fileContent = () => {
+	return JSON.stringify(
+		{
+			rpc: `https://rinkeby.infura.io/v3/${args[0]}`,
+			keys: [`${args[1]}`],
+		},
+		null,
+		2,
+	);
+};
 
-try {
-	const dir = `/home/${require('os').userInfo().username}/.aragon`;
-	if (!fs.existsSync(dir)) {
-		fs.mkdirSync(dir);
-		console.log('created `~/.aragon` file');
-	}
-	fs.writeFile(
-		`/home/${require('os').userInfo().username}/.aragon/rinkeby_key.json`,
-		keySettings,
-		(err) => {
+const saveFile = () => {
+	try {
+		const dir = `/home/${userName}/.aragon`;
+		if (!fs.existsSync(dir)) {
+			fs.mkdirSync(dir);
+			console.log('created `~/.aragon` file');
+		}
+		console.log(fileContent());
+		fs.writeFile('./rinkeby_key.json', fileContent(), (err) => {
 			if (err) {
 				console.log('Error writing file', err);
 			} else {
 				console.log('Successfully setup keys');
 			}
-		},
-	);
-} catch (err) {
-	console.error(err);
-	process.exit(-1);
-}
+		});
+	} catch (err) {
+		console.error(err);
+		process.exit(-1);
+	}
+};
+saveFile();
+module.export = {fileContent, saveFile};
