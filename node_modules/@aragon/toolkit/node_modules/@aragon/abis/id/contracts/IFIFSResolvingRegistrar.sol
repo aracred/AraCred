8e08ee4917c019e10d29581d3fc907015a8129dc
pythonpete32 <pythonpete32@gmail.com>
2020-04-09 10:42:46 +0100

    add .gitignore

--- node_modules/@aragon/toolkit/node_modules/@aragon/abis/id/contracts/IFIFSResolvingRegistrar.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.4.24;
-
-import "./ens/IPublicResolver.sol";
-
-
-interface IFIFSResolvingRegistrar {
-    function register(bytes32 _subnode, address _owner) external;
-    function registerWithResolver(bytes32 _subnode, address _owner, IPublicResolver _resolver) public;
-}
