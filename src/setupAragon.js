38a3cf38ff36bf07f2f0d50c7d3cd328ddac6f71
Enrique Ortiz <hi@enriqueortiz.dev>
2020-04-16 22:07:14 -0400

    Add better linting & configs

--- src/setupAragon.js
+++ src/setupAragon.js
@@ -5,38 +5,38 @@ const userName = require('os').userInfo().username;
 
 const {INFURA_KEY, PRIVATE_KEY} = process.env;
 const fileContent = () => {
-	return JSON.stringify(
-		{
-			rpc: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
-			keys: [`${PRIVATE_KEY}`],
-		},
-		null,
-		2,
-	);
+  return JSON.stringify(
+    {
+      rpc: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+      keys: [`${PRIVATE_KEY}`],
+    },
+    null,
+    2,
+  );
 };
 
 const saveFile = () => {
-	try {
-		const dir = `/home/${userName}/.aragon`;
-		if (!fs.existsSync(dir)) {
-			fs.mkdirSync(dir);
-			console.log('created `~/.aragon` file');
-		}
-		fs.writeFile(
-			`/home/${userName}/.aragon/mainnet_key.json`,
-			fileContent(),
-			(err) => {
-				if (err) {
-					console.log('Error writing file', err);
-				} else {
-					console.log('Successfully setup keys');
-				}
-			},
-		);
-	} catch (err) {
-		console.error(err);
-		process.exit(-1);
-	}
+  try {
+    const dir = `/home/${userName}/.aragon`;
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+      console.log('created `~/.aragon` file');
+    }
+    fs.writeFile(
+      `/home/${userName}/.aragon/mainnet_key.json`,
+      fileContent(),
+      (err) => {
+        if (err) {
+          console.log('Error writing file', err);
+        } else {
+          console.log('Successfully setup keys');
+        }
+      },
+    );
+  } catch (err) {
+    console.error(err);
+    process.exit(-1);
+  }
 };
 
 saveFile();
