8f95a24644524fce30205b915ac0a285a0668a1f
pythonpete32 <pythonpete32@gmail.com>
2020-04-18 19:01:37 +0100

    normalises SourceCred Scores for fixed minting schedule

--- src/processGrain.js
+++ src/processGrain.js
@@ -7,6 +7,9 @@ const book = require('../config/addressbook.json');
 const data = require('../scores.json');
 const transaction = require('../config/dao.json');
 
+// ***Hardcoded*** ammount of tokens to mint per period
+const tokensToMint = 100;
+
 /**
  * This function says hello.
  * @param score Element of `scores.json`[1].users array
@@ -30,20 +33,31 @@ const getScore = (score, addressBook) => {
 
 /**
  * This function says hello.
- * @param rawScore
+ * @param rawScore scores as calculated by Sourcecres
  * @param addressBook `addressbook.json`
- * @param tx
+ * @param tx template tx
  * @returns transation settings
  */
 const mintSettings = (rawScore, addressBook, tx) => {
   const {users} = rawScore[1];
-
   const whitelistedGrain = users
     .filter((leaf) => getScore(leaf, addressBook))
     .map((leaf) => getScore(leaf, addressBook));
 
+  // ----------------------------------------------------------------
+  const total = whitelistedGrain
+    .map((score) => score[1])
+    .reduce((acc, val) => BigNumber(acc).plus(BigNumber(val)).toString());
+
+  const normalisedGrain = whitelistedGrain.map((user) => [
+    user[0],
+    BigNumber(user[1]).dividedBy(total).multipliedBy(tokensToMint).toString(),
+  ]);
+  // console.log(normalisedGrain);
+  // ----------------------------------------------------------------
+
   const settings = tx;
-  settings[0].mints = whitelistedGrain; // <-- maybe broken
+  settings[0].mints = normalisedGrain;
 
   return JSON.stringify(settings, null, 2);
 };
