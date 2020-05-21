25a324b3aca3b5345d340ec14ddfee2bbcd0becc
pythonpete32 <pythonpete32@gmail.com>
2020-05-21 11:38:56 +0100

    add temp payments.json

--- src/transfer.js
+++ src/transfer.js
@@ -1,5 +1,5 @@
-const { encodeCallScript } = require('@aragon/test-helpers/evmScript')
-const { encodeActCall } = require('@aragon/toolkit')
+const {encodeCallScript} = require('@aragon/test-helpers/evmScript');
+const {encodeActCall} = require('@aragon/toolkit');
 
 const {
   daoAddress,
@@ -7,35 +7,35 @@ const {
   financeAddress,
   payments,
   environment,
-} = require('./payments.json')
+} = require('../log/payments.json');
 
 async function main() {
   // Encode a bunch of payments.
   const newImmediatePaymentSignature =
-    'newImmediatePayment(address,address,uint256,string)'
+    'newImmediatePayment(address,address,uint256,string)';
   const calldatum = await Promise.all(
-    payments.map(({ tokenAddress, receiverAddress, amount, receipt }) =>
+    payments.map(({tokenAddress, receiverAddress, amount, receipt}) =>
       encodeActCall(newImmediatePaymentSignature, [
         tokenAddress,
         receiverAddress,
         amount,
         receipt,
-      ])
-    )
-  )
+      ]),
+    ),
+  );
 
-  const actions = calldatum.map(calldata => ({
+  const actions = calldatum.map((calldata) => ({
     to: financeAddress,
     calldata,
-  }))
+  }));
 
   // Encode all actions into a single EVM script.
-  const script = encodeCallScript(actions)
+  const script = encodeCallScript(actions);
   console.log(
-    `npx dao exec ${daoAddress} ${votingAddress} newVote ${script} Payments --environment aragon:${environment} `
-  )
+    `npx dao exec ${daoAddress} ${votingAddress} newVote ${script} Payments --environment aragon:${environment} `,
+  );
 
-  process.exit()
+  process.exit();
 }
 
-main()
+main();
